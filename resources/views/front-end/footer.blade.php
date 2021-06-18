43590f3369450a199bd45486b729ad518315f458
MohcinBN <rapmabiz@gmail.com>
2021-06-18 15:15:04 +0100

    Add footer copyright

--- resources/views/front-end/footer.blade.php
+++ resources/views/front-end/footer.blade.php
@@ -4,6 +4,9 @@
             <a class="nav-link active" href="/">Home</a>
         </li>
         <li class="nav-item">
+            <a class="nav-link active" href="/">About</a>
+        </li>
+        <li class="nav-item">
             <a class="nav-link" href="#">Contact</a>
         </li>
         <li class="nav-item">
@@ -13,4 +16,7 @@
             <a class="nav-link" href="#">RSS</a>
         </li>
     </nav>
+    <div class="col-md-12 text-center">
+        made with simplicity and passion by <a href="/">Remoter.ma</a>
+    </div>
 </footer>
