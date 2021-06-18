01ff5374741e9fd91849de213ab93ead976f2579
MohcinBN <rapmabiz@gmail.com>
2021-06-18 13:40:30 +0100

    Add middleware to admin routes

--- routes/web.php
+++ routes/web.php
@@ -24,17 +24,18 @@ Auth::routes();
 Route::get('/dashboard', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 
-
-// job management routes;
-Route::prefix('job')->group(function () {
-    Route::get('/all', [JobController::class, 'index'])->name('job.index');
-    Route::get('/create', [JobController::class, 'create'])->name('job.create');
-    Route::post('/store', [JobController::class, 'store'])->name('job.store');
-    Route::get('/{id}/edit', [JobController::class, 'edit'])->name('job.edit');
-    Route::put('/{id}/update', [JobController::class, 'update'])->name('job.update');
-    Route::delete('/{id}/delete', [JobController::class, 'destroy'])->name('job.destroy');
-    // change status route
-    Route::get('/chnage-status', [JobController::class, 'changeJobStatus'])->name('change-status');
+Route::middleware(['auth'])->group(function () {
+    // job management routes;
+    Route::prefix('job')->group(function () {
+        Route::get('/all', [JobController::class, 'index'])->name('job.index');
+        Route::get('/create', [JobController::class, 'create'])->name('job.create');
+        Route::post('/store', [JobController::class, 'store'])->name('job.store');
+        Route::get('/{id}/edit', [JobController::class, 'edit'])->name('job.edit');
+        Route::put('/{id}/update', [JobController::class, 'update'])->name('job.update');
+        Route::delete('/{id}/delete', [JobController::class, 'destroy'])->name('job.destroy');
+        // change status route
+        Route::get('/chnage-status', [JobController::class, 'changeJobStatus'])->name('change-status');
+    });
 });
 
 // front-end routes 
